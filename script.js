8775b85159265e3f077d1be66ccfc2cb949d2b2c
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-12 09:32:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
-    //Write your code here
+	const radius=document.getElementById('radius').value
+	const r=parseFloat(radius)
+	if(isNaN(r) || r<0) {
+		document.getElementById('volume').value='NaN'
+		return
+	}
+    const volume =4*Math.PI*Math.pow(r,3)/3
+	const rounded=volume.toFixed(4)
+	document.getElementById('volume').value=rounded//Write your code here
   
 } 
 
